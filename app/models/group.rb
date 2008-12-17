175e713ad3f6c6c17fabf17c25b5fc8b238b3310
Liam Morley <liam@carpeliam.com>
2008-12-17 12:14:17 -0500

    moved 'unique' setting from validation to property

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group
   include DataMapper::Resource
 
   property :id,         Serial
-  property :name,       String,   :length => 100, :nullable => false
+  property :name,       String,   :length => 100, :nullable => false, :unique => true
   property :created_at, DateTime, :lazy => true
   property :updated_at, DateTime, :lazy => true
   
@@ -13,7 +13,6 @@ class Group
 
   validates_present     :leader
   validates_with_method :leader_is_member
-  validates_is_unique   :name
 
   has_slug :on => :name, :called => :grouplink
 
