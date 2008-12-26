88db2ebb6ae421d1e5625afcaf8c8e025401db44
Liam Morley <liam@carpeliam.com>
2008-12-26 18:11:47 -0500

    added livequery plugin for easier event binding

--- public/javascripts/groupthink.js
+++ public/javascripts/groupthink.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
-  $("ul.cat_container ul.categories a").click(function(e) {
+//$(document).ready(function() {
+  $("li.column ul a").livequery("click", function(e) {
     if ($(this).parents("li.column").children("p").children("a.pin").hasClass("on")) {
       e.preventDefault();
       indicator = '<li class="column"><a href="#" class="pin">pin</a></li>';
       link = this;
       $.get(this.href, function(html) {
-        $(link).parents("ul.cat_container").after(html);
+        html = '<li class="column">' + html + '</li>';
+        $(link).parents("li.column").after(html);
       });
     }
   });
-  $("ul.cat_container a.pin").click(function(e) {
+  $("ul.cat_container a.pin").livequery("click", function(e) {
     $(this).toggleClass("on");
     $(this).html($(this).hasClass("on") ? "unpin" : "pin");
   });
-});
+//});
